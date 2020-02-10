0218a83650cfb384ebd3e65351a161ab857ba8cb
Jarda Snajdr <jsnajdr@gmail.com>
2020-02-10 14:01:23 +0100

    LocaleSuggestions: don't switch language on mount or update
    The `LocaleSuggestions` component should only display the list of locale
    suggestions, and not be used to switch the current locale. That's done
    in a route handler.

--- client/components/locale-suggestions/index.jsx
+++ client/components/locale-suggestions/index.jsx
@@ -5,22 +5,19 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import QueryLocaleSuggestions from 'calypso/components/data/query-locale-suggestions';
 import Notice from 'calypso/components/notice';
-import { addLocaleToPath, getLanguage } from 'calypso/lib/i18n-utils';
+import { addLocaleToPath } from 'calypso/lib/i18n-utils';
 import getLocaleSuggestions from 'calypso/state/selectors/get-locale-suggestions';
-import { setLocale } from 'calypso/state/ui/language/actions';
 import LocaleSuggestionsListItem from './list-item';
 
 import './style.scss';
 
 export class LocaleSuggestions extends Component {
 	static propTypes = {
-		locale: PropTypes.string,
 		path: PropTypes.string.isRequired,
 		localeSuggestions: PropTypes.array,
 	};
 
 	static defaultProps = {
-		locale: '',
 		localeSuggestions: [],
 	};
 
@@ -28,33 +25,7 @@ export class LocaleSuggestions extends Component {
 		dismissed: false,
 	};
 
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillMount() {
-		let { locale } = this.props;
-
-		if ( ! locale && typeof navigator === 'object' && 'languages' in navigator ) {
-			for ( const langSlug of navigator.languages ) {
-				const language = getLanguage( langSlug.toLowerCase() );
-				if ( language ) {
-					locale = language.langSlug;
-					break;
-				}
-			}
-		}
-
-		this.props.setLocale( locale );
-	}
-
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		if ( this.props.locale !== nextProps.locale ) {
-			this.props.setLocale( nextProps.locale );
-		}
-	}
-
 	dismiss = () => this.setState( { dismissed: true } );
-
-	getPathWithLocale = ( locale ) => addLocaleToPath( this.props.path, locale );
 
 	render() {
 		if ( this.state.dismissed ) {
@@ -78,17 +49,17 @@ export class LocaleSuggestions extends Component {
 		const localeMarkup = usersOtherLocales.map( ( locale ) => {
 			return (
 				<LocaleSuggestionsListItem
-					key={ 'locale-' + locale.locale }
+					key={ locale.locale }
 					locale={ locale }
 					onLocaleSuggestionClick={ this.dismiss }
-					path={ this.getPathWithLocale( locale.locale ) }
+					path={ addLocaleToPath( this.props.path, locale.locale ) }
 				/>
 			);
 		} );
 
 		return (
 			<div className="locale-suggestions">
-				<Notice icon="globe" showDismiss={ true } onDismissClick={ this.dismiss }>
+				<Notice icon="globe" showDismiss onDismissClick={ this.dismiss }>
 					<div className="locale-suggestions__list">{ localeMarkup }</div>
 				</Notice>
 			</div>
@@ -96,9 +67,6 @@ export class LocaleSuggestions extends Component {
 	}
 }
 
-export default connect(
-	( state ) => ( {
-		localeSuggestions: getLocaleSuggestions( state ),
-	} ),
-	{ setLocale }
-)( LocaleSuggestions );
+export default connect( ( state ) => ( {
+	localeSuggestions: getLocaleSuggestions( state ),
+} ) )( LocaleSuggestions );
