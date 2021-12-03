888bed58eb2635c7232099f23efcd7a9d56c7a63
arthur <arthur.chu@automattic.com>
2021-12-03 17:28:00 +0800

    Implement courses footer to handle the completion of courses

--- client/signup/steps/courses/footer.tsx
+++ client/signup/steps/courses/footer.tsx
@@ -1,5 +1,42 @@
-const Footer = () => {
-	return null;
+import { Button } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import React, { useEffect } from 'react';
+
+interface Props {
+	isCourseComplete?: boolean;
+	onCourseComplete: () => void;
+	onStartWriting: () => void;
+}
+
+const CoursesFooter: React.FC< Props > = ( {
+	isCourseComplete,
+	onCourseComplete,
+	onStartWriting,
+} ) => {
+	const translate = useTranslate();
+
+	useEffect( () => {
+		if ( isCourseComplete ) {
+			onCourseComplete();
+		}
+	}, [ isCourseComplete, onCourseComplete ] );
+
+	if ( ! isCourseComplete ) {
+		return null;
+	}
+
+	return (
+		<div className="courses__footer">
+			<div className="courses__footer-content">
+				{ translate(
+					"You did it! Now it's time to put your skills to work and draft your first post."
+				) }
+				<Button className="courses__footer-button" onClick={ onStartWriting }>
+					{ translate( 'Start writing' ) }
+				</Button>
+			</div>
+		</div>
+	);
 };
 
-export default Footer;
+export default CoursesFooter;
--- client/signup/steps/courses/index.tsx
+++ client/signup/steps/courses/index.tsx
@@ -1,21 +1,35 @@
+import { useViewportMatch } from '@wordpress/compose';
 import { useTranslate } from 'i18n-calypso';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import VideosUi from 'calypso/components/videos-ui';
 import StepWrapper from 'calypso/signup/step-wrapper';
-import { saveSignupStep } from 'calypso/state/signup/progress/actions';
+import { saveSignupStep, submitSignupStep } from 'calypso/state/signup/progress/actions';
 import CoursesFooter from './footer';
 import CoursesHeader from './header';
 import './style.scss';
 
 interface Props {
 	stepName: string;
+	goToNextStep: () => void;
 }
 
 export default function CoursesStep( props: Props ): React.ReactNode {
 	const dispatch = useDispatch();
 	const translate = useTranslate();
-	const { stepName } = props;
+	const { stepName, goToNextStep } = props;
+	const isMobile = useViewportMatch( 'small', '<' );
+	const [ isCourseComplete, setIsCourseComplete ] = useState( false );
+	const hideSkip = isMobile && isCourseComplete;
+
+	const onCourseComplete = useCallback( () => setIsCourseComplete( true ), [
+		setIsCourseComplete,
+	] );
+
+	const onStartWriting = () => {
+		dispatch( submitSignupStep( { stepName } ) );
+		goToNextStep();
+	};
 
 	React.useEffect( () => {
 		dispatch( saveSignupStep( { stepName } ) );
@@ -26,9 +40,22 @@ export default function CoursesStep( props: Props ): React.ReactNode {
 			className="courses"
 			isFullLayout
 			hideFormattedHeader
-			stepContent={ <VideosUi headerBar={ <CoursesHeader /> } footerBar={ <CoursesFooter /> } /> }
+			stepContent={
+				<VideosUi
+					headerBar={ <CoursesHeader /> }
+					footerBar={
+						<CoursesFooter
+							onCourseComplete={ onCourseComplete }
+							onStartWriting={ onStartWriting }
+						/>
+					}
+				/>
+			}
+			hideSkip={ hideSkip }
+			hideNext={ ! hideSkip }
 			skipLabelText={ translate( 'Draft your first post' ) }
 			skipButtonAlign="top"
+			nextLabelText={ translate( 'Start writing' ) }
 			{ ...props }
 		/>
 	);
