cce394964d213c1017a257977172a467278d237d
retrofox <rdsuarez@gmail.com>
2021-12-15 18:37:41 -0300

    woop: add back button when transferring something gets wrong

--- client/signup/steps/woocommerce-install/transfer/index.tsx
+++ client/signup/steps/woocommerce-install/transfer/index.tsx
@@ -1,12 +1,12 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import { useSelector } from 'react-redux';
 import StepWrapper from 'calypso/signup/step-wrapper';
 import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
+import { getSiteDomain } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import InstallPlugins from './install-plugins';
 import TransferSite from './transfer-site';
 import type { WooCommerceInstallProps } from '../';
-
 import './style.scss';
 
 export default function Transfer( props: WooCommerceInstallProps ): ReactElement | null {
@@ -14,19 +14,23 @@ export default function Transfer( props: WooCommerceInstallProps ): ReactElement
 	const siteId = useSelector( getSelectedSiteId ) as number;
 	const isAtomic = useSelector( ( state ) => isAtomicSite( state, siteId ) );
 
+	const domain = useSelector( ( state ) => getSiteDomain( state, siteId ) );
+	const [ hasFailed, setHasFailed ] = useState( false );
+
 	return (
 		<StepWrapper
 			className="transfer__step-wrapper"
 			flowName="woocommerce-install"
-			hideBack={ true }
+			hideBack={ ! hasFailed }
 			hideNext={ true }
 			hideSkip={ true }
 			hideFormattedHeader={ true }
 			isWideLayout={ props.isReskinned }
+			backUrl={ hasFailed ? `/woocommerce-installation/${ domain }` : null }
 			stepContent={
 				<>
 					{ isAtomic && <InstallPlugins /> }
-					{ ! isAtomic && <TransferSite /> }
+					{ ! isAtomic && <TransferSite onFailure={ () => setHasFailed( true ) } /> }
 				</>
 			}
 			{ ...props }
